5d32433dcd3e5f87642ca6c5751dfcc0bcffb44d
arwildo <arwildo@gmail.com>
2019-08-08 18:53:06 +0800

    Update timeClickerBot.py

--- timeClickerBot.py
+++ timeClickerBot.py
@@ -6,13 +6,13 @@ mouse = Controller()
 
 # function that will be execute
 def download():
-    mouse.position = (1024, 288)
+    mouse.position = (873, 307)
     mouse.press(Button.left)
     mouse.release(Button.left)
 
 #the time configuration
 x = datetime.today()
-y = x.replace(day=x.day, hour=x.hour, minute=x.minute+1, second=0, microsecond=0)
+y = x.replace(day=x.day+1, hour=2, minute=9, second=0, microsecond=0)
 delta_t = y-x
 secs = delta_t.seconds+1
 
