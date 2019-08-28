faa024849040dc4e8a83dbee164d8801b6e23736
arwildo <arwildo@gmail.com>
2019-08-28 18:28:55 +0800

    add shebang for unix

--- timeClickerBot.py
+++ timeClickerBot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from datetime import datetime
 from threading import Timer
 from pynput.mouse import Button, Controller
@@ -10,13 +11,15 @@ def download():
     mouse.press(Button.left)
     mouse.release(Button.left)
 
-#the time configuration
-x = datetime.today()
-y = x.replace(day=x.day+1, hour=2, minute=9, second=0, microsecond=0)
-delta_t = y-x
-secs = delta_t.seconds+1
 
-
-# the trigger/count down
-t = Timer(secs, download)
-t.start()
+##the time configuration
+#x = datetime.today()
+#y = x.replace(day=x.day+1, hour=2, minute=9, second=0, microsecond=0)
+#delta_t = y-x
+#secs = delta_t.seconds+1
+#
+#
+## the trigger/count down
+#t = Timer(secs, download)
+#t.start()
+download()
--- timeClickerBotWithInput.py
+++ timeClickerBotWithInput.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from datetime import datetime
 from threading import Timer
 from pynput.mouse import Button, Controller
