04aac539e20d3636d0d20071eb8c0d576aa1db58
arwildo <arwildo@gmail.com>
2019-09-15 23:45:38 +0800

    add new script

--- doubleTimeClickerBot.py
+++ doubleTimeClickerBot.py
@@ -18,14 +18,14 @@ mouse = Controller()
 #
 # function that will be execute
 def download():
-	mouse.position = (mp)
+	mouse.position = (107, 523)
 	mouse.press(Button.left)
 	mouse.release(Button.left)
 
 
 #the time configuration
 x = datetime.today()
-y = x.replace(day=x.day+1, hour=2, minute=9, second=0, microsecond=0)
+y = x.replace(day=x.day+1, hour=2, minute=1, second=0, microsecond=0)
 delta_t = y-x
 secs = delta_t.seconds+1
 
--- timeClickerBotWithInput.py
+++ timeClickerBotWithInput.py
@@ -23,7 +23,7 @@ def runScript():
 
 #the time configuration
 x = datetime.today()
-y = x.replace(day=x.day+1, hour=2, minute=1, second=0, microsecond=0)
+y = x.replace(day=x.day+1, hour=2, minute=15, second=0, microsecond=0)
 delta_t = y-x
 secs = delta_t.seconds+1
 
