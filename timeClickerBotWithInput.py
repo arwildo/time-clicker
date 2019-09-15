75939ef16ad68c6ed2c721260dff717ff868ea98
arwildo <arwildo@gmail.com>
2019-09-15 13:00:40 +0800

    add package

--- timeClickerBotWithInput.py
+++ timeClickerBotWithInput.py
@@ -14,7 +14,7 @@ def runScript():
     mouse.release(Button.left)
 
     # keyboard input here
-    keyboard.type('apt update -y && apt upgrade -y && apt dist-upgrade -y && apt update -y')
+    keyboard.type('apt update -y && apt upgrade -y && apt dist-upgrade -y && apt update -y && apt install seclists -y')
 
     # keyboard key press after input
     keyboard.press(Key.enter)
