7093cc449eb32bc4acc02d71da08848452f58b7f
arwildo <arwildo@gmail.com>
2019-09-17 23:23:06 +0800

    fix script

--- timeClickerBot.py
+++ timeClickerBot.py
@@ -7,25 +7,25 @@ mouse = Controller()
 
 # get mouse position
 def getMp():
-	print('===========')
-	print('timeClicker')
-	print('===========')
-	print('1. Set your mouse position, drag your mouse and hit enter when you done.')
-	input('Enter...')
-	mp = format(mouse.position)
-	print('position = '+ mp)
-	print('Mouse will be clicking that coordinate at the given setting time...')
-	return mp
+    print('===========')
+    print('timeClicker')
+    print('===========')
+    print('1. Set your mouse position, drag your mouse and hit enter when you done.')
+    input('Enter...')
+    mp = format(mouse.position)
+    print('position = '+ mp)
+    print('Mouse will be clicking that coordinate at the given setting time...')
+    return mp
 
 # function that will be execute
 def download():
-    mp = mouse.position	
+    mp = mouse.position 
     mouse.press(Button.left)
     mouse.release(Button.left)
 
 
 #the time configuration
-x = datetime.today()
+x = datetime.today()#NOTE: still need to configure time
 y = x.replace(day=x.day+0, hour=0, minute=1, second=0, microsecond=0)
 delta_t = y-x
 secs = delta_t.seconds+1
