9931e697c4bc6a66185d082da9339263448239ea
arwildo <arwildo@gmail.com>
2019-10-13 23:12:40 +0800

    add timeClickerCountBot ;

--- timeClickerBot.py
+++ timeClickerBot.py
@@ -2,37 +2,25 @@
 from datetime import datetime
 from threading import Timer
 from pynput.mouse import Button, Controller
+from pynput.keyboard import Key, Controller as ck
 
 mouse = Controller()
-
-# get mouse position
-def getMp():
-    print('===========')
-    print('timeClicker')
-    print('===========')
-    print('1. Set your mouse position, drag your mouse and hit enter when you done.')
-    input('Enter...')
-    global mp
-    mp = format(mouse.position)
-    print('position = '+ mp)
-    print('Mouse will be clicking that coordinate at the given setting time...')
+keyboard = ck()
 
 # function that will be execute
-def download():
-    mouse.position = mp
+def runScript():
+    mouse.position = (422, 323) 
     mouse.press(Button.left)
     mouse.release(Button.left)
 
 
-##the time configuration
-#x = datetime.today()
-#y = x.replace(day=x.day+1, hour=2, minute=1, second=0, microsecond=0)
-#delta_t = y-x
-#secs = delta_t.seconds+1
-secs = 3
+#the time configuration
+x = datetime.today()
+y = x.replace(day=x.day+1, hour=2, minute=1, second=0, microsecond=0)
+delta_t = y-x
+secs = delta_t.seconds+1
 
 
 # the trigger/count down
-getMp()
-t = Timer(secs, download)
+t = Timer(secs, runScript)
 t.start()
---
+++ timeClickerCountBot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from datetime import datetime
+from threading import Timer
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+# get mouse position
+def getMp():
+    print('===========')
+    print('timeClicker')
+    print('===========')
+    print('1. Set your mouse position, drag your mouse and hit enter when you done.')
+    input('Enter...')
+    global mp
+    mp = format(mouse.position)
+    print('position = '+ mp)
+    print('Mouse will be clicking that coordinate at the given setting time...')
+
+# function that will be execute
+def download():
+    mouse.position = mp
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+
+
+##the time configuration
+#x = datetime.today()
+#y = x.replace(day=x.day+1, hour=2, minute=1, second=0, microsecond=0)
+#delta_t = y-x
+#secs = delta_t.seconds+1
+secs = 3
+
+
+# the trigger/count down
+getMp()
+t = Timer(secs, download)
+t.start()
